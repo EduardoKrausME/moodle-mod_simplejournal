6317c1a2d5d6365f386d9ab3c8e814738ca7f167
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-14 21:13:38 -0300

    V 1.1.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = "mod_simplejournal";
 $plugin->version = 2026091400;
+$plugin->release = "1.1.0";
+$plugin->component = "mod_simplejournal";
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.0.0";
